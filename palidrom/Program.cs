d8fbc3659ae0babb263800a383babf989263132f
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-09-03 18:27:14 +0300

    коммент с решением по образцу

--- palidrom/Program.cs
+++ palidrom/Program.cs
@@ -36,3 +36,38 @@ public class Answer
         System.Console.WriteLine($"{result}");
     }
 }
+
+/* решение из образца
+using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number){
+        if (number < 10000 || number >= 100000){
+            Console.WriteLine("Число не пятизначное");
+            return false;
+        }
+
+        int reverse = 0;
+        int temp = number;
+        while (temp > 0){
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+        return reverse == number;
+    }
+
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+            number = 645464;
+        }
+
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
+*/
