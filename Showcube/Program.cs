b0b25e594dff7d39e975c9374e21e33eea68f195
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-09-03 18:28:30 +0300

    коммент с решением по образцу

--- Showcube/Program.cs
+++ Showcube/Program.cs
@@ -30,3 +30,35 @@ public class Answer
         ShowCube(N);
     }
 }
+
+/*
+using System;
+
+public class Answer
+{
+    static void ShowCube(int N)
+    {
+        for (int i = 1; i <= N; i++)
+        {
+            Console.WriteLine(Cube(i));
+        }
+    }
+
+    public static int Cube(int number)
+    {
+        return number * number * number;
+    }
+
+      static public void Main(string[] args) {
+        int N;
+
+        if (args.Length >= 1) {
+            N = int.Parse(args[0]);
+        } else {
+            N = 6;
+        }
+
+        ShowCube(N);
+    }
+}
+*/
